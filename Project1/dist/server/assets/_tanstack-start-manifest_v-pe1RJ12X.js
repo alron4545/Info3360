87ca4c7b5083a1d54e10da68434f1548e641ed5d
Michael Schuler <schuler.michael90@gmail.com>
2026-08-30 22:40:05 -0700

    more things

---
+++ Project1/dist/server/assets/_tanstack-start-manifest_v-pe1RJ12X.js
@@ -0,0 +1,25 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/Michaelschuler/School/Fall2026/INFO 3360 (Server Side Web Frameworks)/Project1/src/routes/__root.tsx",
+		children: ["/", "/about"],
+		preloads: ["/assets/index-Ct3Kql8_.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-Ct3Kql8_.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/Michaelschuler/School/Fall2026/INFO 3360 (Server Side Web Frameworks)/Project1/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-C0Q7ySsr.js"]
+	},
+	"/about": {
+		filePath: "/Users/Michaelschuler/School/Fall2026/INFO 3360 (Server Side Web Frameworks)/Project1/src/routes/about.tsx",
+		children: void 0,
+		preloads: ["/assets/about-RPkB35Pf.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
